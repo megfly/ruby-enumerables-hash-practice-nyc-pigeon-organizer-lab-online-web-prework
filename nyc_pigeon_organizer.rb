8d04b976e56fcf75459361b7b0949a50d8bc8877
Meghan Flynn <mflynn333@gmail.com>
2019-11-19 23:29:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,17 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.each do |key, values|
-    names.each do |name|
-      pigeon_list[name]
+  data.each do |attributes, value|
+    value.each do |stats, all_names|
+      all_names.each do |name|
+        if pigeon_list[name] == nil 
+          pigeon_list[name] = {} 
+        end 
+        if pigeon_list[name][attributes] == nil 
+          pigeon_list[name][attributes] = []
+        end 
+        pigeon_list[name][attributes].push(stats.to_s)
+      end 
+    end 
+  end 
+  pigeon_list
 end
